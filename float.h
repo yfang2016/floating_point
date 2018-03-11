b33b786efc947f9efe4c86562bb43fee6c1bc0d5
yfang1644 <yfang1644@gmail.com>
2018-03-11 21:57:45 +0800

    create float.h

---
+++ float.h
@@ -0,0 +1,27 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  float.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/07/2018 06:56:01 PM
+ *       Revision:  none
+ *       Compiler: 
+ *
+ *         Author:  Fang Yuan (yfang@nju.edu.cn)
+ *   Organization:  nju
+ *
+ * ============================================================================
+ */
+
+#ifndef _FLOAT_H
+#define _FLOAT_H
+
+typedef struct {
+    unsigned int frac: 24;
+    char exp: 8;
+} Float;
+
+#endif
